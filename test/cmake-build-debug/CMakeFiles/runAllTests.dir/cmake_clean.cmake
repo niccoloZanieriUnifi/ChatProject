2525cf475b19797c2203e99697d952aaf8111c6e
niccolo <niccolo.zanieri.13@gmail.com>
2020-08-18 17:46:13 +0200

    Setup unit test directory

---
+++ test/cmake-build-debug/CMakeFiles/runAllTests.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+        "CMakeFiles/runAllTests.dir/runAllTests.cpp.o"
+        "runAllTests"
+        "runAllTests.pdb"
+        )
+
+# Per-language clean rules from dependency scanning.
+foreach (lang CXX)
+    include(CMakeFiles/runAllTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach ()
