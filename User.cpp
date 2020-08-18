66dcf740d433583bd14062fa8e4c4bd5fe8fdbb9
niccolo <niccolo.zanieri.13@gmail.com>
2020-08-18 18:10:04 +0200

    Added User constructor

--- User.cpp
+++ User.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "User.h"
+
+User::User(std::string n, std::string &s, std::string &nick) : name(std::move(n)), surname(std::move(s)),
+                                                               nick(std::move(nick)) {}
+
+void User::addFriend(const User &newFriend) {
+
+}
+
+
+
--- User.h
+++ User.h
@@ -6,7 +6,25 @@
 #define CHATPROJECT_USER_H
 
 
+#include <string>
+#include <list>
+#include <utility>
+
 class User {
+public:
+    User(std::string n, std::string &s, std::string &nick);
+
+    User(const User &original) = delete;
+
+    User &operator=(const User &rhs) = delete;
+
+    void addFriend(const User &newFriend);
+
+private:
+    std::string name;
+    std::string surname;
+    std::string nick;
+    std::list<std::string> friends;
 
 };
 
